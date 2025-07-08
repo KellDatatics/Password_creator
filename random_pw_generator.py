39ff771a85efc06afdc61e8f145a7f8f116afe7b
Kell McClen <kmmcclen@gmail.com>
2025-07-08 17:43:13 -0500

    Add files via upload

---
+++ random_pw_generator.py
@@ -0,0 +1,29 @@
+#Mini Password Generator
+
+
+import random 
+
+capital_let = "QWERTYUIOPASDFGHJKLZXCVBNM" 
+small_let = "qwertyuiopasdfghjklzxcvbnm"
+numbers = "1234567890" 
+special_char = "!@#$%^&*<>|"
+
+upper,lower,digits,odd = True,True,True,True 
+finalpass = "" 
+
+
+if upper: 
+    finalpass += capital_let
+if lower:
+    finalpass += small_let 
+if digits:
+    finalpass += numbers
+if odd:
+    finalpass += special_char
+
+length = 20 
+howmuch = 10 
+
+for x in range(howmuch):
+    password = "".join(random.sample(finalpass,length))
+    print(password)
